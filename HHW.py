c369304dd8208592dcd04e51b699ac0ddc28c0a0
yoboy1102 <156419541+yoboy1102@users.noreply.github.com>
2024-01-14 03:46:45 +0400

    Add files via upload

--- HHW.py
+++ HHW.py
@@ -1,7 +1,7 @@
 while True:
     print()
     print("-------------------------------------------------------------------------------------------------------------------------------------------")
-    print(" \t \t \t \t \t \t \tWELCOME TO ABC")
+    print(" \t \t \t \t \t \t \tWELCOME TO SSA AIRWAYS")
     print("-------------------------------------------------------------------------------------------------------------------------------------------")
     print()
 
@@ -32,29 +32,34 @@ while True:
                     print("Domestic Flights Are Not Supported")
                     print("Press 'ENTER' to try again")    
                     input()
-                print(f"Your  departure point is {dept} \nYour arrival point is {arv}")
-                print()
-                conf_1 = input("Enter 1 to Confirm \nEnter any key to Register again \n===> ")
-                print()
-
-                if conf_1 == '1':
-                    print("Departure & Arrival points Confirmed!")
-                    print("Press 'ENTER' to continue")
-                    input()
-                    break
-
+                    print()
                 else:
-                    continue
+
+                    print(f"Your  departure point is {dept} \nYour arrival point is {arv}")
+                    print()
+                    conf_1 = input("Enter 1 to Confirm \nEnter any key to Register again \n===> ")
+                    print()
+
+                    if conf_1 == '1':
+                        print("Departure & Arrival points Confirmed!")
+                        print("Press 'ENTER' to continue")
+                        input()
+                        break
+
+                    else:
+                        continue
             else:
                 print("Error, Invalid input.")
                 print("Press 'ENTER' to try again")
                 input()
+                print()
 
     
         else:
             print("Error, Invalid input.")
             print("Press 'ENTER' to try again")
             input()
+            print()
             
     print("============================================================================================================================================")
     print()
@@ -70,6 +75,7 @@ while True:
             print('Error, Invalid input.')
             print("Press 'ENTER' to try again")
             input()
+            print()
 
         else:
             n_kids = int(input("Enter Number of Kids (Under-16) in the Passenger`s list:-- "))
@@ -85,6 +91,7 @@ while True:
                 print("Number of seats not available:")
                 print("Press 'ENTER' to Register again")
                 input()
+                print()
 
             else:
                 conf_1 = input("Enter 1 to confirm \nEnter any key to Register again \n===> ")
@@ -93,6 +100,7 @@ while True:
                     print("Confirmed!")
                     print("Press 'ENTER' to continue")
                     input()
+                    print()
                     break
                 else:
                     continue
@@ -130,6 +138,7 @@ while True:
             print('Error, Invalid input.')
             print("Press 'ENTER' to try again")
             input()
+            print()
     while True:
         #Date
         if month == 'April':
@@ -148,7 +157,7 @@ while True:
             print("ERROR, Invalid input")
             print("Press 'ENTER' to try again")
             input()
-        
+            print()
         
         else:
             DD,MM,YY = str(date),str(ind_s),str(year)
@@ -158,10 +167,12 @@ while True:
             print(f"Your Departure date is {d_date}, in the format {d_form}")
             print()
             conf_1 = input("Enter 1 to confirm \nEnter any key to Register again \n===> ")
+            print()
             if conf_1 == '1':
                 print("Date Confirmed!")
                 print("Press 'ENTER' to continue")
                 input()
+                print()
                 break
             else:
                 continue
@@ -169,9 +180,10 @@ while True:
     print("============================================================================================================================================")
     print()
 
-        #AVAILIBLITY OF FLIGHTS
+    #AVAILIBLITY OF FLIGHTS
     while True:
         print("FLIGHTS AVAILIBILITY")
+        print()
         print("Currencies accepted ==>")
         L_cur = ['AED','USD','INR','GPB','EUR']
         L_ncur = ['Dirhams','American Dollars','Rupees','British Pounds','Euros']
@@ -179,7 +191,7 @@ while True:
         hr1,hr2,hr3,hr4,hr5 = 8,7,6,3,3
 
         for i in range(len(L_cur)):
-            print(f"{L_ncur} ({L_cur[i]})")
+            print(f"{L_ncur[i]} ({L_cur[i]})")
 
         cur = input("First, Please enter your preffered currency(by the ISO code):- ").upper()
         if cur in L_cur:
@@ -196,17 +208,15 @@ while True:
                 pr1,pr2,pr3,pr4,pr5 = 1.27*pr1,1.27*pr2,1.27*pr3,1.27*pr4,1.27*pr5
                 break 
             elif cur == 'EUR':
-                pr1,pr2,pr3,pr4,pr5 = 1.16*pr1,1.16*pr2,1.16*pr3,1.16*pr4,1.16*pr5
+                pr1,pr2,pr3,pr4,pr5 = 1.15*pr1,1.15*pr2,1.15*pr3,1.15*pr4,1.15*pr5
                 break
         else:
             print('ERROR, Invalid input')
             print("Press 'ENTER' to try again")
             input()
-            continue        
-        
-
-        
-                
+            print()
+            continue
+            
     while True:        
         from asciiSS import ALL, PLA
         al=ALL
@@ -218,117 +228,194 @@ while True:
         f='A'
 
         print(f)
-        print('--------------------------------------------------------------------------')
-        print(f'|\t\t',hr1,f'hour',f"\t\t",f'Over-lay \t|')
+        print('------------------------------------------------------------------------')
+        print(f'|\t\t',hr1,f'hour',f"\t\t",f'Over-lay  \t\t\t|')
         for i in range(4):
         
             print('|','\t',ALL[ind_d],'\t',pl[i],'\t',ALL[ind_a],'\t','|')
             ind_d+=1
             ind_a+=1 
-        print(f'|\t\t{pr1}',cur,f"\t\t",f'ECONOMY \t|')
-        print('--------------------------------------------------------------------------')
+        print(f'|\t\t{pr1}',cur,f"\t\t",f'ECONOMY  \t\t\t|')
+        print('------------------------------------------------------------------------')
 
         ind_d = air.index(dept)
         ind_a = air.index(arv)
         f='B'
 
         print(f)
-        print('--------------------------------------------------------------------------')
-        print(f'|\t\t',hr2,f'hour',f"\t\t",f'Over-lay \t|')
+        print('------------------------------------------------------------------------')
+        print(f'|\t\t',hr2,f'hour',f"\t\t",f'Over-lay  \t\t\t|')
         for i in range(4):
         
             print('|','\t',ALL[ind_d],'\t',pl[i],'\t',ALL[ind_a],'\t','|')
             ind_d+=1
             ind_a+=1 
-        print(f'|\t\t{pr2}',cur,f"\t\t",f'ECONOMY \t|')
-        print('--------------------------------------------------------------------------')
+        print(f'|\t\t{pr2}',cur,f"\t\t",f'ECONOMY  \t\t\t|')
+        print('------------------------------------------------------------------------')
 
         ind_d = air.index(dept)
         ind_a = air.index(arv)
         f='C'
 
         print(f)
-        print('--------------------------------------------------------------------------')
-        print(f'|\t\t',hr3,f'hour',f"\t\t",f'Over-lay \t|')
+        print('------------------------------------------------------------------------')
+        print(f'|\t\t',hr3,f'hour',f"\t\t",f'Over-lay  \t\t\t|')
         for i in range(4):
         
             print('|','\t',ALL[ind_d],'\t',pl[i],'\t',ALL[ind_a],'\t','|')
             ind_d+=1
             ind_a+=1 
-        print(f'|\t\t{pr3}',cur,f"\t\t",f'ECONOMY \t|')
-        print('--------------------------------------------------------------------------')
+        print(f'|\t\t{pr3}',cur,f"\t\t",f'ECONOMY  \t\t\t|')
+        print('------------------------------------------------------------------------')
 
         ind_d = air.index(dept)
         ind_a = air.index(arv)
         f='D'
 
         print(f)
-        print('--------------------------------------------------------------------------')
-        print(f'|\t\t',hr4,f'hour',f"\t\t",f'Over-lay \t|')
+        print('------------------------------------------------------------------------')
+        print(f'|\t\t',hr4,f'hour',f"\t\t",f'Over-lay  \t\t\t|')
         for i in range(4):
         
             print('|','\t',ALL[ind_d],'\t',pl[i],'\t',ALL[ind_a],'\t','|')
             ind_d+=1
             ind_a+=1 
-        print(f'|\t\t{pr4}',cur,f"\t\t",f'ECONOMY \t|')
-        print('--------------------------------------------------------------------------')
+        print(f'|\t\t{pr4}',cur,f"\t\t",f'ECONOMY  \t\t\t|')
+        print('------------------------------------------------------------------------')
 
         ind_d = air.index(dept)
         ind_a = air.index(arv)
         f='E'
 
         print(f)
-        print('--------------------------------------------------------------------------')
-        print(f'|\t\t',hr5,f'hour',f"\t\t",f'Over-lay \t|')
+        print('------------------------------------------------------------------------')
+        print(f'|\t\t',hr5,f'hour',f"\t\t",f'Over-lay  \t\t\t|')
 
         for i in range(4):
         
             print('|','\t',ALL[ind_d],'\t',pl[i],'\t',ALL[ind_a],'\t','|')
             ind_d+=1
             ind_a+=1 
-        print(f'|\t\t{pr5}',cur,f"\t\t",f'ECONOMY \t|')
-        print('--------------------------------------------------------------------------')
+        print(f'|\t\t{pr5}',cur,f"\t\t",f'ECONOMY  \t\t\t|')
+        print('------------------------------------------------------------------------')
 
-        select=input('Select Flight(by the letter assigned to it):-- ').upper()
-        break
+        select = input('Select Flight(by the letter assigned to it):-- ').upper()
+        print()
+        conf_1 = input("Enter 1 to confirm \nEnter any key to Register Again \n==> ")
+        if conf_1 == '1':
+            print("Confirmed")
+            print("Press 'Enter' to continue")
+            input()
+            print()    
+            break
+        else:
+            continue
+    print("============================================================================================================================================")
+    print()
 
-#seat reservation 
-import random
-t = [['A', 'B', 'C'],['D', 'E', 'F'],['A', 'B', 'C'],['D', 'E', 'F'],['A', 'B', 'C'],['D', 'E', 'F'],['A', 'B', 'C'],['D', 'E', 'F'],['A', 'B', 'C'],['D', 'E', 'F'],['A', 'B', 'C'],['D', 'E', 'F'],['A', 'B', 'C'],['D', 'E', 'F']]
 
-for i in range(0, 7):
-    g = random.randint(0, 7)
-    f = random.randint(0, 2)
-    t[g][f] = 'X'
-print(f''''
-  
-1.{t[0]}--b1       {t[1]}---b2
-2.{t[2]}--b3       {t[3]}---b4
-3.{t[4]}--b5       {t[5]}---b6
-4.{t[6]}--b8       {t[7]}---b9
-5.{t[8]}--b10      {t[9]}---b11
-6.{t[10]}--b13      {t[11]}---b14
-7.{t[12]}--b15      {t[13]}---b16
-*seats marked with X are already reserved
-*rows: 1,2,3,4,5,6,7
-*columns: A,B,C,D,E,F
-*blocks: 1 to 16
-''')
+    while True:
+        #SEAT RESERVATION
+        L = [['|A1|', '|B1|', '|C1|', '|D1|', '|E1|', '|F1|'], ['|A2|', '|B2|', '|C2|', '|D2|', '|E2|', '|F2|'], ['|A3|', '|B3|', '|C3|', '|D3|', '|E3|', '|F3|'], ['|A4|', '|B4|', '|C4|', '|D4|', '|E4|', '|F4|'], ['|A5|', '|B5|', '|C5|', '|D5|', '|E5|', '|F5|'], ['|A6|', '|B6|', '|C6|', '|D6|', '|E6|', '|F6|'], ['|A7|', '|B7|', '|C7|', '|D7|', '|E7|', '|F7|']]
+        
+        xx='|XX|'
+        print('','  A ','  B ','  C ',' ','  D ','  E ','  F ')
+        n = 1
+        for i in range(len(L)):
+            for m in range(1):
+                print(n,L[i][m], L[i][m+1], L[i][m+2],' ',L[i][m+3], L[i][m+4], L[i][m+5])
+                n += 1
+        nmp = n_pass
+        n2 = 1
+        n3 = 1
+        n4 = 0
+        if nmp == 1:
+            sea_t = input('Choose your seat from the available options:-- ')
+            line = '|'
+            seat = line + sea_t + line
+            print(seat)
+            conf_1 = input("Enter 1 to confirm \nEnter any key to Register again \n==> ")
+            if conf_1 == '1':
+                print()
+                print("Seat reserved")
+                print("Press 'ENTER' to continue")
+                input()
+                for i in range(7):
+                    if seat in L[i]:
+                        i2 = i
+                    else:
+                        n4 += 1
+                if n4 >= 7:
+                    print("ERROR, Invalid input")
+                    print("Press 'Enter' to try again")
+                    input()
+                    print()
+                else:
+                
+                    L2=L[i2]         
+                    ind_st = L2.index(seat)        
+                    L2[ind_st] = xx     
+                    L[i2] = L2
+                    break
+                
+            else:
+                print("Press 'ENTER' to Register again")
+                input()
+                print()
+                        
+        else:
+            for j in range(nmp):
+                n4 = 0
+                n2=1
+                sea_t=input(f'Choose the seat for passenger {n2}:-- ')
+                line = '|'
+                seat = line + sea_t + line
+                print(seat)
+                n2 += 1
 
-num = 3
-for i in range(num):
-    row = int(input("please select row number: "))
-    column = input("please select column number(A,B,C,D,E,F): ").upper()
-    block = int(input("enter block number: "))
+                conf_1 = input("Enter 1 to confirm \nEnter any key to Register again \n==> ")
+                if conf_1 == '1':
+                    print()
+                    print("Seat reserved")
+                    print("Press 'ENTER' to continue")
+                    input()
+                    for i in range(7):
+                        if seat in L[i]:
+                            i2 = i
+                
+                        else:
+                            n4 += 1
+                    if n4 >= 7:
+                        print("ERROR, Invalid input")
+                        print("Press 'Enter' to continue")
+                        input()
+                        print()
+                    else:
+                        L2=L[i2] 
+                        ind_st = L2.index(seat)        
+                        L2[ind_st] = xx
+                        L[i2] = L2
+                        print('','  A ','  B ','  C ',' ','  D ','  E ','  F ')
+                        for i in range(len(L)):
+                            for m in range(1):
+                                    
+                                    print(n3,L[i][m], L[i][m+1], L[i][m+2],' ',L[i][m+3], L[i][m+4], L[i][m+5])
+                                    n3 += 1
+                
 
-    if column not in t[block-1]:
-        print("sorry seat is already reserved please select another seat press enter key to try again")
-        num+=1
-        input()
-    else:
-        ind = t[block - 1].index(column)
-        t[block-1][ind] = 'X'
-        print(f"seat number = {column}{row}")
+                elif conf_1 != '1':
+                    print("Press 'ENTER' to Register again")
+                    input()
+                    print()
+                    break
+                if j+1 == nmp:
+                    break
+                
+                
+
+
+
+        
 
 
         
